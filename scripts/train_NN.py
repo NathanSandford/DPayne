2473842b125ceafcaf8aab2ade4bdc37d20a27a3
NathanSandford <nathan_sandford@berkeley.edu>
2019-09-26 17:04:18 -0700

    new training script for use with The_Payne

--- scripts/train_NN.py
+++ scripts/train_NN.py
@@ -1,26 +1,40 @@
 import sys
 sys.path.append('/global/scratch/nathan_sandford/DPayne/')
+from pathlib import Path
 import pandas as pd
 from DPayne.utils import split_data
+from DPayne.neuralnetworks import read_in_neural_network
 from The_Payne import training
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+input_dir = Path('/global/scratch/nathan_sandford/kurucz/kurucz_out/synthetic_spectra/HMC_valid/R6500')
+nn_default_file = Path('NN_normalized_spectra.pt')
+NN_output_file = Path('/global/scratch/nathan_sandford/DPayne/NN_data/NN_R6500.pt')
+model_param_output_file = Path('/global/scratch/nathan_sandford/DPayne/NN_data/NN_R6500_par.npz')
 
 # Sample Settings
 frac_train = 0.75
 randomize = True
 random_state = 3457
 element_labels = ['Fe', 'Ca', 'Ni', 'Si', 'Ti', 'Co', 'Mg']
+
 labels_to_train_on = ['Teff', 'logg', 'v_micro'] + element_labels
 
 # NN Settings
 num_neurons = 300
-num_steps = 1e5
+num_steps = 1e3 #1e5
+num_features = 64*5
+mask_size = 11
 learning_rate = 0.001
 
-spectra = pd.read_hdf(input_file, 'spectra')
-labels = pd.read_hdf(input_file, 'labels')
+
+spec_files = [spec_file for spec_file in input_dir.iterdir()]
+spectra = pd.DataFrame()
+labels = pd.DataFrame()
+for spec_file in spec_files:
+    spectra = pd.concat([spectra, pd.read_hdf(spec_file, 'spectra')], axis=1)
+    labels = pd.concat([labels, pd.read_hdf(spec_file, 'labels')], axis=1)
+spectra.columns = np.arange(spectra.shape[1])
+labels.columns = np.arange(labels.shape[1])
 labels.loc[set(labels.index) ^ {'Teff', 'logg', 'v_micro', 'Fe'}] -= labels.loc['Fe']
 
 training_labels, training_spectra, validation_labels, validation_spectra = split_data(spectra, labels,
@@ -33,6 +47,18 @@ training_loss, validation_loss = training.neural_net(training_labels, training_s
                                                      validation_labels, validation_spectra,
                                                      num_pixel=training_spectra.shape[1],
                                                      num_neurons=num_neurons, num_steps=num_steps,
+                                                     num_features=num_features, mask_size=mask_size,
                                                      learning_rate=learning_rate)
+nn_default_file.rename(NN_output_file)
+
+model_params = dict(dim_in=training_labels.shape[1],
+                    num_neurons=num_neurons,
+                    num_features=num_features,
+                    mask_size=mask_size,
+                    num_pixel=training_spectra.shape[1])
+np.savez(model_param_output_file, **model_params)
+
+
+
 
 print('Training Complete!')
